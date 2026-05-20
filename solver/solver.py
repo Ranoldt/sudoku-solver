b298435f392ec03269083f97d8cee113156e30fe
Ranoldt <117882837+Ranoldt@users.noreply.github.com>
2026-05-19 18:22:43 -0700

    get_candidates per space

--- solver/solver.py
+++ solver/solver.py
@@ -7,4 +7,15 @@ class LogicSolver:
         self.puzzle = puzzle
 
     def get_candidates(self) -> List[List[List[int]]]:
-        pass
+        candidates = []
+        for r in range(9):
+            row_candidates = []
+            for c in range(9):
+                cand = []
+                if self.puzzle.board[r][c] != 0:
+                    b = self.puzzle.get_boxIndex(r,c) 
+                    seen = self.puzzle.rows[r] + self.puzzle.columns[c] + self.puzzle.boxes[b]
+                    cand = [i for i in range(1,10) if i not in seen]
+                row_candidates.append(cand)
+            candidates.append(row_candidates)
+        return candidates
